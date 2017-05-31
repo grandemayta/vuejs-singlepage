1a18b740e57ca17594f85b894ff59c49807a7861
Gabriel Anderson Mayta Trigozo <a59m@mbclm010.local>
2017-05-31 12:55:57 +0200

    initial structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const Webpack                                  = require('webpack');
 const CleanWebpackPlugin                       = require('clean-webpack-plugin');
 const HtmlWebpackPlugin                        = require("html-webpack-plugin");
-const path  	                               = require('path');
+const path  	                                 = require('path');
 const appPath                                  = path.join(__dirname, './app');
 const distPath                                 = path.join(__dirname, './dist');
 
@@ -10,10 +10,10 @@ module.exports = {
 	entry: {
 		app: appPath + '/core',
 		vendor: [
-            'vue',
-            'vue-router',
+      'vue',
+      'vue-router',
 			'vue-resource'
-        ]
+    ]
 	},
 	output: {
 		path: distPath,
@@ -22,48 +22,48 @@ module.exports = {
 	module: {
 		rules: [
 			{
-		        test: /\.vue$/,
-		        loader: 'vue-loader',
+		  	test: /\.vue$/,
+		    loader: 'vue-loader',
 				options: {
 					loaders: {
 						'scss': 'vue-style-loader!css-loader!sass-loader'
 					}
 				},
 				exclude: /node_modules/
-	      	},
+	    },
 			{
 				test: /\.js$/,
 				loader: 'babel-loader',
 				exclude: /node_modules/
-			},
+			}
 		]
 	},
 	devServer: {
-        historyApiFallback: true
-    },
-    plugins: [
+		historyApiFallback: true
+  },
+	plugins: [
 
-        new HtmlWebpackPlugin({
-            template: 'index.template.ejs',
-            inject: true,
-            hash: true
-        }),
+			new HtmlWebpackPlugin({
+					template: 'index.template.ejs',
+					inject: true,
+					hash: true
+			}),
 
-        new Webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            filename: 'bundle.vendor.js',
-            minChunks: Infinity
-        })
+			new Webpack.optimize.CommonsChunkPlugin({
+					name: 'vendor',
+					filename: 'bundle.vendor.js',
+					minChunks: Infinity
+			})
 
-    ],
-    resolve: {
+	],
+  resolve: {
 		modules: ['node_modules'],
 		alias: {
 			'vue': 'vue/dist/vue.esm',
 			'configs': appPath + '/core/configs',
-            'features': appPath + '/features',
+      'features': appPath + '/features',
 			'commons': appPath + '/commons'
-        }
+    }
 	}
 };
 
@@ -71,11 +71,11 @@ module.exports = {
 if (process.env.NODE_ENV === 'production') {
   module.exports.plugins = (module.exports.plugins || []).concat([
 
-	new CleanWebpackPlugin([distPath], {
-		root: path.join(__dirname, './'),
-		verbose: true,
-		dry: false
-	}),
+		new CleanWebpackPlugin([distPath], {
+			root: path.join(__dirname, './'),
+			verbose: true,
+			dry: false
+		}),
 
     new Webpack.DefinePlugin({
       'process.env': {
